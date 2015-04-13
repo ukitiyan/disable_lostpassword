6869b26fbcfefca2197496b7d1f6451664735cb0
TAKAHASHI Yuki <y-takahashi@begood-tech.com>
2015-04-13 13:11:59 +0900

    first commit

---
+++ appinfo/app.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * ownCloud - disable_lostpassword
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Begood Technology Corp. <y-takahashi@begood-tech.com>
+ * @copyright Begood Technology Corp. 2014
+ */
+
+namespace OCA\Disable_lostpassword\AppInfo;
+
+\OCP\Util::addStyle('disable_lostpassword', 'style');
